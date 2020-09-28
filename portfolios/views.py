4a8806d02392a963d1516df695ec0702fe667eaa
Sue Holder <susanpholder@gmail.com>
2020-09-28 14:57:58 +0100

    updated portfolio views

--- portfolios/views.py
+++ portfolios/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Portfolio, Bucket
+
+class PortfolioListView(ListView):
+    model = Portfolio
+    template_name = 'portfolio/portfolio_list.html'
+    
+class PortfolioDetailView(DetailView):
+    model = Portfolio
+    template_name = 'portfolio/portfolio_detail.html'
+    
+
+class BucketListView(ListView):
+    model = Bucket
+    template_name = 'portfolio/bucket_list.html'
+    
+class PortfolioDetailView(DetailView):
+    model = Bucket
+    template_name = 'portfolio/bucket_detail.html'
