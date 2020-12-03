7007c262d8a3759aa954f54b939fe039f4a2dc68
Sue Holder <susanpholder@gmail.com>
2020-12-03 18:57:51 +0000

    added slugfield to profiles for seo

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.template.defaultfilters import slugify
 
 User = get_user_model()
 
@@ -8,6 +9,7 @@ def user_dir(instance, filename):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    slug = models.SlugField(blank=True)
     avatar = models.ImageField(default='avatar.png',upload_to=user_dir)
     status = models.TextField(max_length=350, default='No status ...')
     relations = models.ManyToManyField(User, related_name='relations', blank=True)
@@ -30,3 +32,20 @@ class Profile(models.Model):
     
     def __str__(self):
         return str(self.user.username)
+    
+    def save(self, *args, **kwargs):
+        add_slug = str(self.user)
+        self.slug = add_slug
+        super().save(*args, **kwargs)
+    
+STATUS_CHOICES = (
+        ('Requested', 'Requested'),
+        ('Confirmed', 'Confirmed')
+)
+
+class FriendRequest(models.Model):
+    to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='to_user')
+    from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='from_user')
+    is_relation = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=9, choices=STATUS_CHOICES)
