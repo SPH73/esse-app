e6d4a28912404e026a9362657d45a6d466a834ef
Sue Holder <susanpholder@gmail.com>
2020-12-09 09:43:43 +0000

    updated admin import to Asset

--- albums/admin.py
+++ albums/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Album, MediaUpload
+from .models import Album, Asset
 
 class AlbumModelAdmin(admin.ModelAdmin):
     list_display = (
@@ -12,5 +12,5 @@ class AlbumModelAdmin(admin.ModelAdmin):
     ordering = ('-updated',)
     
 admin.site.register(Album, AlbumModelAdmin)
-admin.site.register(MediaUpload)
+admin.site.register(Asset)
 
