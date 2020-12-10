82758731f213088e161c06aee62d6b82064ac8a6
Sue Holder <susanpholder@gmail.com>
2020-12-10 17:21:41 +0000

    added profile to link users album in storage

--- albums/views.py
+++ albums/views.py
@@ -6,6 +6,9 @@ from profiles.models import Profile
 from .forms import AssetModelForm, CreateAlbumModelForm
 
 def gallery(request):
+    """
+    Render a view to create and display a gallery of users albums
+    """
     profile = get_object_or_404(Profile, user=request.user)
     albums = profile.albums.all()
     if request.method == 'POST':
@@ -28,18 +31,21 @@ def gallery(request):
     return render(request, template, context)
    
 def album_detail(request, album):
+    """
+    Get the authenticated user profile to generate the cloud storage folder if not already created, use the profile related name to retieve and display the assets stored in a single album, render a form and add the chosen asset to the album.
+    """
     profile = get_object_or_404(Profile, user=request.user)
     albums = profile.albums.all()
+    # get the album
     album = albums.get(slug=album)
-    print(album)
     assets = album.assets.all()
-    asset = None
     if request.method == 'POST':
         asset_form = AssetModelForm(request.POST, request.FILES)
-        print(request.POST, request.FILES)
         if asset_form.is_valid():
             asset = asset_form.save(commit=False)
             asset.album = album
+            asset.profile = profile
+            print(album)
             asset.save()
             messages.success(request, 'Media added successfully')
         else:
