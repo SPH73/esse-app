12d7ff23d5069d9497a3b4497228a4675017ccb5
Sue Holder <susanpholder@gmail.com>
2020-12-14 19:54:06 +0000

    removed signal for now as code not working locally

--- albums/signals.py
+++ albums/signals.py
@@ -3,8 +3,9 @@ from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 from .models import Asset
         
-@receiver(pre_delete, sender=Asset)
-def pre_delete_asset(self, sender, instance, **kwargs):
-    cloudinary.uploader.destroy(instance.asset.public_id)
-    print('Asset deleted from Cloudinary')
+# @receiver(pre_delete, sender=Asset)
+# def pre_delete_asset(self, sender, instance, **kwargs):
+#     print("Delete album signal")
+#     result = cloudinary.uploader.destroy(instance.asset.public_id)
+#     print(result)
         
