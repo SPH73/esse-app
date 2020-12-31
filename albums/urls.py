4d8aaba4a32f99993e45920c23eaeb3a39e3939a
Sue Holder <susanpholder@gmail.com>
2020-12-31 23:13:07 +0000

    fixed album urls not resolving to correct view

--- albums/urls.py
+++ albums/urls.py
@@ -4,30 +4,34 @@ from . import views
 app_name = 'albums'
 urlpatterns = [
     path(
-        'my-portfolio/', views.gallery, name='gallery'
+        'my-portfolios/', views.gallery, name='gallery'
+    ),
+    path(
+        'my-album/<slug:album>/',
+        views.album_detail,
+        name='album_detail'
+    ),
+    path(
+        'my-asset/<slug:album>/delete/',
+        views.album_delete,
+        name='delete_album'
     ),
     path(
         'user-albums/<slug:album>/', views.user_album, name='user_album'
     ),
     path(
-        'user-albums/<slug:album>/<slug:asset>/',
-        views.album_asset,
-        name='album_asset'),
-    path(
-        'my-portfolio/<slug:album>/',
-        views.album_detail,
-        name='album_detail'
-    ),
-    path(
-        'my-portfolio/<slug:album>/delete/',
-        views.album_delete,
-        name='delete_album'
-    ),
-    path(
-        'user-albums/<slug:album>/<slug:asset>/',
+        'user-album/<slug:album>/<slug:asset>/',
         views.asset_detail,
         name='asset_detail'
     ),
+    
+    path(
+        'user-asset/<slug:album>/<slug:asset>/',
+        views.album_asset,
+        name='album_asset'),
+    
+    
+   
     path(
         'my-portfolio/<slug:album>/<slug:asset>/delete/',
         views.asset_delete,
