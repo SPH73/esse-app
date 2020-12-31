b0fc6f4c67458cea44e5f8f2e61d87a18b36526e
Sue Holder <susanpholder@gmail.com>
2020-12-31 08:26:11 +0000

    added custom 404 and 500 error pages

---
+++ config/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def handler404(request, exception):
+    """
+    Page Not Found
+    """
+    return render(request, 'errors/404.html', status=404)
+
+
+def handler500(request):
+    """
+    Internal Server Error
+    """
+    return render(request, 'errors/500.html', status=500)
